c5b99bd174eb2dfc26be92e4851ee2404f69e84a
KKaushik6306 <kaushik.k6306@gmail.com>
2024-11-06 00:17:05 +0530

    Deployment Config

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -16,11 +16,11 @@ class PredictPipeline:
             preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             print("Before Loading")
             print(model_path)
-            #model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
+            model=load_object(file_path=model_path) #Load Model
+            preprocessor=load_object(file_path=preprocessor_path) #Load Preprocessor Object
             print("After Loading")
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
+            data_scaled=preprocessor.transform(features) #Transform New Data
+            preds=model.predict(data_scaled) #Predict based on new data
             return preds
         
         except Exception as e:
